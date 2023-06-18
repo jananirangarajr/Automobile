b9697d8f34f335d2d9474961703c3c5ed98f05a2
Janani <55023218+jananirangarajr@users.noreply.github.com>
2023-06-18 14:12:27 -0500

    Initial commit

---
+++ car.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Cars(BaseModel):
+    VIN: str
+    plate: str
+    state: str
+    Model: int
+    owner_name: str
+    owner_Address: str
+    owner_DL: str
+    problem_description: str
+    date_in: str
+    date_out: str
+    technician: str
+    city: str
+    Zipcode: int
+
+class ModifiedCar(BaseModel):
+    VIN: Optional[str] = None
+    plate: Optional[str] = None
+    state: Optional[str] = None
+    Model: Optional[int] = None
+    owner_name: Optional[str] = None
+    owner_Address: Optional[str] = None
+    owner_DL: Optional[str] = None
+    problem_description: Optional[str] = None
+    date_in: Optional[str] = None
+    date_out: Optional[str] = None
+    technician: Optional[str] = None
+    city: Optional[str] = None
+    Zipcode: Optional[int] = None
