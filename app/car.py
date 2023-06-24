c076c427d4a7a51629364e67fe7c951b2a1c728f
Janani <55023218+jananirangarajr@users.noreply.github.com>
2023-06-24 16:22:13 -0500

    schema changes

--- app/car.py
+++ app/car.py
@@ -13,23 +13,25 @@ class Cars(BaseModel):
     date_in: str
     date_out: str
     technician: str
-    shop_id: int = Field(..., ge=0, le=48)
+    shop_id: str
     shop_name: str
     shop_city: str
     shop_state: str
 
 
 class ModifiedCar(BaseModel):
-    VIN: Optional[str] = None
+    vin: Optional[str] = None
     plate: Optional[str] = None
     state: Optional[str] = None
-    Model: Optional[int] = None
+    model: Optional[int] = None
     owner_name: Optional[str] = None
-    owner_Address: Optional[str] = None
-    owner_DL: Optional[str] = None
+    owner_address: Optional[str] = None
+    owner_dl: Optional[str] = None
     problem_description: Optional[str] = None
     date_in: Optional[str] = None
     date_out: Optional[str] = None
     technician: Optional[str] = None
-    city: Optional[str] = None
-    Zipcode: Optional[int] = None
+    shop_id: Optional[str] = None
+    shop_name: Optional[str] = None
+    shop_city: Optional[str] = None
+    shop_state: Optional[str] = None
